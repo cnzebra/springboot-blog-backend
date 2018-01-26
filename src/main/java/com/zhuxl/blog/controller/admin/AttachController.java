503a28b694c5bfae6530e4f0b8f553fcd4f8cfbf
v5zhu@qq.com <v5zhu880624>
2018-01-26 17:19:44 +0800

    支持多文件同时上传

--- src/main/java/com/zhuxl/blog/controller/admin/AttachController.java
+++ src/main/java/com/zhuxl/blog/controller/admin/AttachController.java
@@ -1,5 +1,6 @@
 package com.zhuxl.blog.controller.admin;
 
+import com.github.pagehelper.PageInfo;
 import com.zhuxl.blog.constant.WebConst;
 import com.zhuxl.blog.controller.BaseController;
 import com.zhuxl.blog.dto.LogActions;
@@ -11,13 +12,15 @@ import com.zhuxl.blog.service.IAttachService;
 import com.zhuxl.blog.service.ILogService;
 import com.zhuxl.blog.utils.Commons;
 import com.zhuxl.blog.utils.TaleUtils;
-import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +28,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -96,6 +100,58 @@ public class AttachController extends BaseController {
                     errorFiles.add(fname);
                 }
             }
+        } catch (Exception e) {
+            return RestResponseBo.fail();
+        }
+        return RestResponseBo.ok(errorFiles);
+    }
+
+    /**
+     * 上传多文件接口
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping(value = "upload/multiple")
+    @ResponseBody
+    public RestResponseBo upload(HttpServletRequest request)
+            throws IOException {
+        UserVo users = this.user(request);
+        Integer uid = users.getUid();
+        List<String> errorFiles = new ArrayList<>();
+        try {
+            CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(request.getSession()
+                    .getServletContext());
+            if (commonsMultipartResolver.isMultipart(request)) {
+                MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+                MultiValueMap<String, MultipartFile> multiValueMap = multipartRequest.getMultiFileMap();//图片集合
+                Iterator<String> iterator = multipartRequest.getFileNames();
+                while (iterator.hasNext()) {
+                    //取得上传文件
+                    String fieldName = iterator.next();
+                    List<MultipartFile> multipartFiles = multiValueMap.get(fieldName);
+
+                    for (MultipartFile multipartFile : multipartFiles) {
+                        String fname = multipartFile.getOriginalFilename();
+                        if (multipartFile.getSize() <= WebConst.MAX_FILE_SIZE) {
+                            String fkey = TaleUtils.getFileKey(fname);
+                            String ftype = TaleUtils.isImage(multipartFile.getInputStream()) ? Types.IMAGE.getType()
+                                    : Types.FILE.getType();
+                            File file = new File(CLASSPATH + fkey);
+                            try {
+                                FileCopyUtils.copy(multipartFile.getInputStream(), new FileOutputStream(file));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            attachService.save(fname, fkey, ftype, uid);
+                        } else {
+                            errorFiles.add(fname);
+                        }
+                    }
+                }
+
+            }
+
         } catch (Exception e) {
             return RestResponseBo.fail();
         }

f77520875447c2c09daa2aa4983638ecb74b0169
v5zhu@qq.com <v5zhu880624>
2018-01-29 16:12:45 +0800

    优化

--- src/main/java/com/zhuxl/blog/utils/IPKit.java
+++ src/main/java/com/zhuxl/blog/utils/IPKit.java
@@ -18,7 +18,10 @@ public class IPKit {
      * @return IP Address
      */
     public static String getIpAddrByRequest(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
+        String ip = request.getHeader("X-real-ip");//先从nginx自定义配置获取
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("x-forwarded-for");
+        }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }

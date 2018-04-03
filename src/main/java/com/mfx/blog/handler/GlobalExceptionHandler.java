4ca97baff0acdbbe59f8ca7f118c2400acc12b39
v5zhu@qq.com <v5zhu880624>
2018-04-03 18:53:18 +0800

    添加校验字段

--- src/main/java/com/mfx/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/mfx/blog/handler/GlobalExceptionHandler.java
@@ -3,10 +3,14 @@ package com.mfx.blog.handler;
 import com.mfx.blog.exception.TipException;
 import com.mfx.blog.exception.TipException;
 import com.mfx.blog.exception.TipException;
+import com.mfx.blog.modal.bo.RestResponseBo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author mfx
@@ -17,16 +21,17 @@ public class GlobalExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = TipException.class)
-    public String tipException(Exception e) {
+    @ResponseBody
+    public ResponseEntity tipException(Exception e) {
         LOGGER.error("find exception:e={}", e.getMessage());
-        e.printStackTrace();
-        return "comm/error_500";
+        return new ResponseEntity(RestResponseBo.fail(e.getMessage()), HttpStatus.OK);
     }
 
 
     @ExceptionHandler(value = Exception.class)
-    public String exception(Exception e) {
+    @ResponseBody
+    public ResponseEntity exception(Exception e) {
         LOGGER.error("find exception:e={}", e.getMessage(), e);
-        return "comm/error_404";
+        return new ResponseEntity(RestResponseBo.fail("内部错误"), HttpStatus.OK);
     }
 }
